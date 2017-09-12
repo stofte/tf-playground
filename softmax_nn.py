bee3ac40afe5250fc2fb3a70e07105e682e10058
Svend Tofte <stofte@gmail.com>
2017-09-12 14:02:21 +0200

    initial softmax regression

--- softmax_nn.py
+++ softmax_nn.py
@@ -2,6 +2,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from plotting import show
+import os
+import numpy as np
 import argparse
 import sys
 
@@ -10,7 +13,6 @@ from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 
 FLAGS = None
-
 
 mnist_path = "/mnt/c/src/tf-playground/mnist"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
@@ -23,9 +25,9 @@ def main(_):
     b = tf.Variable(tf.zeros([10]), 'b')
     Y = tf.nn.softmax(tf.matmul(X, W) + b)
 
-    Y_ = tf.placeholder(tf.float32, [10, None], 'Y_')
+    Y_ = tf.placeholder(tf.float32, [None, 10], 'Y_')
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y_, logits=Y))
-    train_step = tf.train.GradientDescentOptimizer(0.003).minimize(cross_entropy)
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
     sess = tf.InteractiveSession()
     init = tf.global_variables_initializer().run()
@@ -37,6 +39,14 @@ def main(_):
     correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     print(sess.run(accuracy, feed_dict={X: mnist.test.images, Y_: mnist.test.labels}))
+    We = W.eval()
+    Wec = [np.reshape(x, (28, 28)) for x in np.hsplit(We, 10)]
+    Wecc = np.block([
+        [Wec[0], Wec[1], Wec[2], Wec[3], Wec[4]], 
+        [Wec[5], Wec[6], Wec[7], Wec[8], Wec[9]] 
+    ])
+    show(Wecc)
+    # print(np.shape(np.block(Wecc)))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
