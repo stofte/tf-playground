7bdc8b49e9ac9288ab528fd9fab02b09e2f389db
Svend Tofte <stofte@gmail.com>
2018-04-12 21:34:00 +0200

    Use saved session to do predictions

--- notebooks/helpers/output_folder.py
+++ notebooks/helpers/output_folder.py
@@ -2,8 +2,8 @@ import os
 import sys
 import datetime
 
-def output_folder(prefix='output-'):
-    relpath = os.path.join(os.path.dirname(__file__), '..', 'logs')
+def output_folder(prefix='output-', folderpath=['logs']):
+    relpath = os.path.join(os.path.dirname(__file__), '..', '..', *folderpath)
     ts = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
     f = '{}{}'.format(prefix, ts)
     return os.path.abspath(os.path.join(relpath, f))
